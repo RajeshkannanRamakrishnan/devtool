28fa0f5cd99c69626b1f8bdd3e0120e358a7314e
Rajeshkannan R <rrajeshkannanbca@gmail.com>
2025-12-21 20:21:39 +0530

    feat: add md5 tool

---
+++ cmd/md5.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var upperMd5 bool
+
+// md5Cmd represents the md5 command
+var md5Cmd = &cobra.Command{
+	Use:   "md5",
+	Short: "Computes the MD5 hash of the input string",
+	Long: `Computes the MD5 hash of the given string arguments. 
+If the --upper flag is used, the output will be in uppercase.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		input := strings.Join(args, " ")
+		hash := md5.Sum([]byte(input))
+		hashString := hex.EncodeToString(hash[:])
+		if upperMd5 {
+			hashString = strings.ToUpper(hashString)
+		}
+		fmt.Println(hashString)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(md5Cmd)
+	md5Cmd.Flags().BoolVarP(&upperMd5, "upper", "u", false, "Output hash in uppercase")
+}
