4388cd2bd280ac6f4cc95c5c0d3bbf5d3e7dc1cb
Rajeshkannan R <rrajeshkannanbca@gmail.com>
2026-01-01 21:23:14 +0530

    feat: add server option to server static file

---
+++ cmd/server.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var serverPort int
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start a static file server",
+	Long: `Start a static HTTP file server for the current directory.
+You can specify the port using the --port (or -p) flag.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("Error getting current directory: %v", err)
+		}
+
+		fs := http.FileServer(http.Dir(cwd))
+		mux := http.NewServeMux()
+		mux.Handle("/", fs)
+
+		addr := fmt.Sprintf(":%d", serverPort)
+		fmt.Printf("Serving %s at http://localhost%s\n", cwd, addr)
+		
+		if err := http.ListenAndServe(addr, mux); err != nil {
+			log.Fatalf("Server failed: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+	serverCmd.Flags().IntVarP(&serverPort, "port", "p", 8080, "Port to listen on")
+}
